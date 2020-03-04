1dace1f80297bf8c2280a2159fb26bb256f403d5
emilandi <emilandi2000@hotmail.com>
2020-03-04 09:29:58 -0300

    Comentarios

--- getRate.py
+++ getRate.py
@@ -5,15 +5,20 @@ from datetime import datetime
 
 system('cls')
 
+#obtener fecha y hora
 now = datetime.now().strftime("%Y/%m/%d ~ %H:%M:%S")
 
+#titulo
 print('')
 print('   Cotizacion Oficial Dolar')
 
+#url
 URL = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales'
 
+#respuesta
 json = requests.get(URL).json()
 
+#header
 print()
 print('  MONEDA  |  COMPRA  |  VENTA')
 print('  ---------------------------')
@@ -26,10 +31,12 @@ for index, emoji in enumerate(('🟢', '🔵')):
     	moneda='USD '
     if nombre=='Dolar Blue' :
     	moneda='BLUE'
-
+    
+    #datos
     compra = json[index]['casa']['compra'][:-1]
     venta = json[index]['casa']['venta'][:-1]
    
+    #salida
     print(f"  {moneda}    |   {compra}  |  {venta}")
 
 print('  ---------------------------')
